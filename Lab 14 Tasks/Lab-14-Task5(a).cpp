248924b95fdc3177feb3b96422f341eaf67fff54
Hashir Asad <145664273+WeWeBunnyX@users.noreply.github.com>
2024-01-12 20:36:24 +0500

    Update Lab-14-Task5(a).cpp

--- Lab 14 Tasks/Lab-14-Task5(a).cpp
+++ Lab 14 Tasks/Lab-14-Task5(a).cpp
@@ -8,6 +8,8 @@ int main() {
 
     outputFile << "\n66 33 16";
     outputFile << "\n13 29 12";
+    
+    outputFile.close();
 
     cout << "Data has been written to the file 'num.txt'." << endl;
 
