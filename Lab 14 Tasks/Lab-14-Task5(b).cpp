bf799a9954d1c30532272ca22b24bfd096fb9843
Hashir Asad <145664273+WeWeBunnyX@users.noreply.github.com>
2024-01-12 20:26:38 +0500

    Create Lab-14-Task5(b).cpp

---
+++ Lab 14 Tasks/Lab-14-Task5(b).cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <fstream>
+
+using namespace std;
+
+int main() {
+    ofstream outputFile("sum.txt");
+
+    int num1, num2, num3, sum;
+
+    cout << "Enter three numbers separated by spaces: ";
+    while (cin >> num1 >> num2 >> num3) {
+        sum = num1 + num2 + num3;
+        outputFile << sum << endl;
+
+        cout << "Enter three numbers separated by spaces: ";
+    }
+
+    cout <<"Check 'sum.txt' for results." << endl;
+
+    return 0;
+}
