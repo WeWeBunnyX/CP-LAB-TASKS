502aca34e9d39425f56aa7794c2b59a7d553205f
Hashir Asad <145664273+WeWeBunnyX@users.noreply.github.com>
2024-01-12 20:19:57 +0500

    Create Lab-14-Task5(a).cpp

---
+++ Lab 14 Tasks/Lab-14-Task5(a).cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <fstream>
+
+using namespace std;
+
+int main() {
+    ofstream outputFile("num.txt");
+
+    outputFile << "\n66 33 16";
+    outputFile << "\n13 29 12";
+
+    cout << "Data has been written to the file 'num.txt'." << endl;
+
+    return 0;
+}
